a5252928f29eb1168ba89b2d8fe31c75f65df573
Shoham <shoamivgi1234@gmail.com>
2025-01-19 15:23:12 +0200

    s

--- src/main/java/com/Supplify/Supplify/Services/SupplierService.java
+++ src/main/java/com/Supplify/Supplify/Services/SupplierService.java
@@ -1,27 +1,27 @@
 package com.Supplify.Supplify.Services;
 
 import com.Supplify.Supplify.entities.BusinessSupplier;
+import com.Supplify.Supplify.repositories.BusinessSupplierRepo;
 import com.Supplify.Supplify.repositories.SupplierRepo;
-import com.Supplify.Supplify.repositories.BusinessSupplier_Repo;
-import com.Supplify.Supplify.repositories.BusinessRepo;
 import com.Supplify.Supplify.entities.Supplier;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-import java.util.Set;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
 public class SupplierService {
     private final SupplierRepo supplierRepo;
-    private final BusinessSupplier_Repo businessSupplierRepo;  // Added this repository
+    private final BusinessSupplierRepo businessSupplierRepo;
 
-    public void createSupplier(Supplier supplier, Integer businessId) {
-        // First save the supplier
+    @Transactional
+    public Supplier createSupplier(Supplier supplier, Integer businessId) {
+
         Supplier savedSupplier = supplierRepo.save(supplier);
-        // Create and save the business-supplier relationship
+
         BusinessSupplier businessSupplier = new BusinessSupplier(businessId, savedSupplier.getSupplierId());
         businessSupplierRepo.save(businessSupplier);
+
+        return savedSupplier;
     }
 }
